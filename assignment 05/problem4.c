c4ab3799bf035cf78eb1ca199a4f25d41870b629
yh_21 <yh_21@naver.com>
2025-04-20 02:09:17 +0900

    .

---
+++ assignment 05/problem4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Recursive function to compute GCD
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main()
+{
+    int x, y;
+    printf("Enter two integers: ");
+    scanf("%d %d", &x, &y);
+
+    int result = gcd(x, y);
+    printf("GCD is %d\n", result);
+
+    return 0;
+}
