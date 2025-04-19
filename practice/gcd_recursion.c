ef87bf29a3b63e19e45b286cc29bb847c4c32517
yh_21 <yh_21@naver.com>
2025-04-20 00:30:56 +0900

    .

---
+++ practice/gcd_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int gcd(int x, int y)
+{
+    if (y == 0)
+        return x;
+    else
+        return (gcd(y, x % y));
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("gcd of %d, %d is %d", a, b, gcd(a, b));
+
+    return 0;
+}
