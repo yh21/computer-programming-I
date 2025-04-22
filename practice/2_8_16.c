7d74e31762aeb807da06d5ce59d82f7ac45f5f57
yh_21 <yh_21@naver.com>
2025-04-22 13:02:43 +0900

    .

---
+++ practice/2_8_16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    int y = 010;
+    int z = 0x10;
+
+    printf("%d", x);
+    printf("%d", y);
+    printf("%d", z);
+}
---
+++ practice/over.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    float value = 0.1;
+
+    printf("%.20f\n", value);
+    return 0;
+}
