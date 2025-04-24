8857955edbc29bb5bfe8860698ee58b24a07bb89
yh_21 <yh_21@naver.com>
2025-04-25 00:45:36 +0900

    .

---
+++ practice/capital.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char letter;
+
+    while(1)
+    {
+    printf("소문자를 입력하시오: ");
+    scanf(" %c", &letter);
+
+    if (letter == 'Q')
+        break;
+    if (letter < 'a' || letter > 'z')
+        continue;
+
+    letter = letter - 32;
+    printf("capital is %c\n", letter);
+    }
+}
