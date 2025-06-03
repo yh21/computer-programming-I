63834a3b91710d2da3c1bcfca6244347b47215a2
yh_21 <yh_21@naver.com>
2025-06-03 16:27:47 +0900

    .

---
+++ practice2/pointer_conversion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int data = 0x0A0B0C0D;
+    char *pc;
+    
+    pc = (char *)&data;
+    for (int i = 0; i < 4; i++)
+        printf("*(pc + %d) = %02X\n", i, *(pc + i));
+
+    return 0;
+}
