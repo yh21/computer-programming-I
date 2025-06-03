6a2128d83984d6b4dbf3abe645f00b9f9bc5d32b
yh_21 <yh_21@naver.com>
2025-06-03 16:58:33 +0900

    .

--- practice2/slope.c
+++ practice2/slope.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+int get_line(int x1, int x2, int y1, int y2, double *slope, double *yintercept);
 
 int main()
 {
+    double s, y;
+    if (get_line(3, 3, 6, 6, &s, &y) == -1)
+        printf("error");
+    else
+        printf("slope is %lf, yintercept is %lf", s, y);
+
     return 0;
+}
+
+int get_line(int x1, int y1, int x2, int y2, double *slope, double *yintercept)
+{
+    if (x1 == x2)
+        return -1;
+    else
+    {
+        *slope = (double)(y2 - y1) / (double)(x2 - x1);
+        *yintercept = y1 - (*slope) * x1;
+        return 0;
+    }
 }
