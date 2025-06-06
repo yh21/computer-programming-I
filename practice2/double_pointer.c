1270f12e6d8ac4fa7942360cde85744756d4a169
yh_21 <yh_21@naver.com>
2025-06-06 13:28:40 +0900

    add encryption.c file

---
+++ practice2/double_pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void set_pointer(char **q);
+
+int main()
+{
+    char *p;
+    set_pointer(&p);
+
+    printf("%s", p);
+    return 0;
+}
+
+void set_pointer(char **q)
+{
+    *q = "hi";
+}
