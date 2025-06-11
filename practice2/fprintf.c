5412af5a922bf8521a7a544f6567088c5f84d216
yh_21 <yh_21@naver.com>
2025-06-11 21:05:51 +0900

    fprintf.c

---
+++ practice2/fprintf.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    char fname[100];
+    int number = 0, count = 0;
+    char name[20];
+    double score = 0, total = 0;
+
+    printf("Enter score file name: ");
+    scanf("%s", fname);
+
+    if ((fp = fopen(fname, "w")) == NULL)
+    {
+        fprintf(stderr, "Can't open the score file %s\n", fname);
+        exit(1);
+    }
+
+    while (1)
+    {
+        printf("Enter student number, name and score: (if the student number is not negative, end)");
+        scanf("%d", &number);
+        if (number < 0)
+            break;
+        scanf("%s %lf", name, &score);
+        fprintf(fp, "%d %s %lf", number, name, score);
+    }
+    fclose(fp);
+
+    if((fp = fopen(fname, "r")) == NULL)
+    {
+        fprintf(stderr, "Can't open the score file %s\n", fname);
+        exit(1);
+    }
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d %s %lf", &number, name, &score);
+        total += score;
+        count ++;
+    }
+
+    printf("Average: %lf", total / count);
+    fclose(fp);
+    return 0;
+
+}
